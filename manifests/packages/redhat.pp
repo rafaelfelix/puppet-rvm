2c5f83cda638a80ca396b9ba2c2128af378daf02
Rafael Felix Correa <rafael.felix@gmail.com>
2012-03-09 15:22:07 -0300

    updated dependency packages for CentOS

--- manifests/packages/redhat.pp
+++ manifests/packages/redhat.pp
@@ -1,7 +1,8 @@
 class rvm::packages::redhat {
-  $package_list = ['gcc-c++', 'patch', 'readline', 'readline-devel', 'zlib', 
-                   'zlib-devel', 'libyaml-devel', 'libffi-devel', 
-                   'openssl-devel', 'make', 'bzip2']
+  $package_list = ['which', 'gcc', 'gcc-c++', 'make', 'gettext-devel', 'expat-devel',  
+    'curl-devel', 'zlib-devel', 'openssl-devel', 'perl', 'cpio', 'expat-devel', 'gettext-devel',
+	'wget', 'bzip2', 'sendmail', 'mailx', 'libxml2', 'libxml2-devel', 'libxslt', 'libxslt-devel',
+	'readline-devel', 'patch', 'git']
                    
   # Virtualize Package list to prevent conflicts
   @package { $package_list:
