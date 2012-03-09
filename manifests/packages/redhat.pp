75d0845b3f1dc68d4561936c2dc9f9963d5b2ab1
Rafael Felix Correa <rafael.felix@gmail.com>
2012-03-09 15:44:16 -0300

    removed duplicated package expat-devel

--- manifests/packages/redhat.pp
+++ manifests/packages/redhat.pp
@@ -1,7 +1,7 @@
 class rvm::packages::redhat {
   $package_list = ['which', 'gcc', 'gcc-c++', 'make', 'gettext-devel', 'expat-devel',  
-    'curl-devel', 'zlib-devel', 'openssl-devel', 'perl', 'cpio', 'expat-devel', 'gettext-devel',
-	'wget', 'bzip2', 'sendmail', 'mailx', 'libxml2', 'libxml2-devel', 'libxslt', 'libxslt-devel',
+    'curl-devel', 'zlib-devel', 'openssl-devel', 'perl', 'cpio', 'gettext-devel', 'wget',
+	'bzip2', 'sendmail', 'mailx', 'libxml2', 'libxml2-devel', 'libxslt', 'libxslt-devel',
 	'readline-devel', 'patch', 'git']
                    
   # Virtualize Package list to prevent conflicts
